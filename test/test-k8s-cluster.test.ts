bf31fc66a7172942bb03117fb9b3cfe842f4c15e
Oge Nnadi <oge@nnadi.org>
2024-06-07 14:59:11 -0500

    Initial commit

---
+++ test/test-k8s-cluster.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestK8SCluster from '../lib/test-k8s-cluster-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test-k8s-cluster-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestK8SCluster.TestK8SClusterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
